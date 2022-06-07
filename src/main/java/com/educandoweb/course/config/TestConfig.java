b7695a5513f229e5d56f35aa678955ecbdf930aa
Edson Junior <edson_junior91@hotmail.com>
2022-06-07 17:21:17 -0300

    Order, Instant, ISO 8601 - PART2

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.educandoweb.course.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.educandoweb.course.entities.Order;
 import com.educandoweb.course.entities.Usuario;
+import com.educandoweb.course.repositories.OrderRepository;
 import com.educandoweb.course.repositories.UsuarioRepository;
 /*Para fazer com que execute a associação da instancia usuarioRepository com a 
  * a Classe TestConfig, é necessário implementar a interface CommandLineRunner*/
@@ -24,15 +27,23 @@ public class TestConfig implements CommandLineRunner {
 	 */
 	@Autowired
 	private UsuarioRepository usuarioRepository;
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
 	@Override
 	public void run(String... args) throws Exception {
 
 		Usuario u1 = new Usuario(null, "Maria Brown", "maria@gmail.com", "98888888", "123456");
 		Usuario u2 = new Usuario(null, "Alex Green", "alex@gmail.com", "9777777", "123456");
 		
+		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
+		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
+		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+		
 		/*Para salvar os usuários no banco de dados, vou chamar o usuarioRepository*/
 		usuarioRepository.saveAll(Arrays.asList(u1,u2));
+		orderRepository.saveAll(Arrays.asList(o1,o2,o3));
 		
 		
 	}
