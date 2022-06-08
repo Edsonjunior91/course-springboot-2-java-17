48db6ddbe052908a90d1c3c778c1fec9c81e51ab
Edson Junior <edson_junior91@hotmail.com>
2022-06-08 10:58:50 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/educandoweb/course/entities/Product.java
+++ src/main/java/com/educandoweb/course/entities/Product.java
@@ -9,8 +9,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
-import javax.persistence.Transient;
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable{
@@ -33,7 +35,11 @@ public class Product implements Serializable{
 	 * ai utiliza a Classe HashSet correspondente a essa interface
 	 * Da mesma forma quando utilizamos o LIST instanciamos com o ArrayList.. 
 	 * Essa parte faz conexão entre 2 entidades e no caso */
-	@Transient  /*Essa Anotation da JPA impede que JPA tente interpretar essa parte*/
+	  /*@Transient Essa Anotation da JPA impede que JPA tente interpretar essa parte*/
+	@ManyToMany
+	@JoinTable(name = "tb_product_category", 
+	joinColumns = @JoinColumn(name = "product_id"), 
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<>();
 	
 	public Product() {
